13b8007a310b7d486b252a9daaab44334f449159
Ayanavo <ayanavo2057@gmail.com>
2023-02-26 00:51:00 +0530

    format

--- src/app/Modules/chat/chat.module.ts
+++ src/app/Modules/chat/chat.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ChatRoutingModule } from './chat-routing.module';
 
-
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ChatRoutingModule
-  ]
+	declarations: [],
+	imports: [CommonModule, ChatRoutingModule, FormsModule],
 })
-export class ChatModule { }
+export class ChatModule {}
