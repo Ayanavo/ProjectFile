52cdf36d1733936c713bf84ec13b3e81d53a074e
Ayanavo <ayanavo2057@gmail.com>
2023-02-26 18:11:47 +0530

    chatbot changes

---
+++ src/app/Modules/chat/Components/chat-dialog/chat-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'chat-dialog',
+	templateUrl: './chat-dialog.component.html',
+	styleUrls: ['./chat-dialog.component.scss'],
+})
+export class ChatDialogComponent {}
--- src/app/Modules/chat/chat.module.ts
+++ src/app/Modules/chat/chat.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ChatRoutingModule } from './chat-routing.module';
+import { ChatService } from './chat.service';
+import { ChatDialogComponent } from './Components/chat-dialog/chat-dialog.component';
 
 @NgModule({
-	declarations: [],
+	declarations: [ChatDialogComponent],
 	imports: [CommonModule, ChatRoutingModule, FormsModule],
+	exports: [ChatDialogComponent],
+	providers: [ChatService],
 })
 export class ChatModule {}
