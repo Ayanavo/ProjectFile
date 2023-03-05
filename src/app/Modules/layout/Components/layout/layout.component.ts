5111b20f03e81032e507820bd5d343f43b3280a0
Ayanavo <ayanavo2057@gmail.com>
2023-03-05 18:55:38 +0530

    chat ui setup

--- src/app/Modules/layout/Components/layout/layout.component.ts
+++ src/app/Modules/layout/Components/layout/layout.component.ts
@@ -1,3 +1,4 @@
+import { Overlay } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ChatDialogComponent } from 'src/app/Modules/shared/chat-dialog/chat-dialog.component';
@@ -8,11 +9,22 @@ import { ChatDialogComponent } from 'src/app/Modules/shared/chat-dialog/chat-dia
 	styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent {
-	constructor(public dialog: MatDialog) {}
+	constructor(public dialog: MatDialog, private overlay: Overlay) {}
 
 	openDialog(): void {
+		const scrollStrategy = this.overlay.scrollStrategies.reposition();
 		const dialogRef = this.dialog.open(ChatDialogComponent, {
-			width: '250px',
+			autoFocus: false,
+			width: '350px',
+			height: '420px',
+			disableClose: true,
+			position: {
+				bottom: '50px',
+				right: '50px',
+			},
+			backdropClass: 'backdropBackground',
+			panelClass: 'dialogContainer',
+			scrollStrategy,
 		});
 
 		dialogRef.afterClosed().subscribe((result) => {
--- src/app/Modules/shared/chat-dialog/chat-dialog.component.ts
+++ src/app/Modules/shared/chat-dialog/chat-dialog.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
 	selector: 'chat-dialog',
 	templateUrl: './chat-dialog.component.html',
 	styleUrls: ['./chat-dialog.component.scss'],
 })
-export class ChatDialogComponent {
-	constructor() {}
+export class ChatDialogComponent implements OnInit {
+	public chat = new FormControl('');
+	public globaltime: string = new Date().toLocaleTimeString('en-gb', {
+		hour12: false,
+	});
+	public globaldate: string = new Date().toLocaleDateString('en-gb');
+	public Master: any[] = [{ date: this.globaldate, msg: [] }];
+	i = 0;
+	public activetext: any = '';
+
+	ngOnInit(): void {
+		this.Master = JSON.parse(
+			localStorage.getItem('chat_log') || '[{ date: this.globaldate, msg: [] }]'
+		);
+		this.chat.valueChanges.subscribe((x) => {
+			this.activetext = x;
+		});
+	}
+
+	getTextAll() {
+		if (this.Master[0]['msg'].length === 0) {
+			this.Master[0]['msg'].push({
+				text: this.chat.value,
+				time: this.globaltime,
+			});
+		} else {
+			////now the fun begins
+			if (this.globaldate == this.Master[this.i]['date']) {
+				this.Master[this.i]['msg'].push({
+					text: this.chat.value,
+					time: this.globaltime,
+				});
+			} else {
+				this.Master.push({ date: this.globaldate, msg: [] });
+
+				this.Master[this.i + 1]['msg'].push({
+					text: this.chat.value,
+					time: this.globaltime,
+				});
+				this.i += 1;
+			}
+		}
+		console.log(this.Master);
+
+		// this.chatArray.push({ text: this.chat.value, time: new Date() });
+		this.chat.reset();
+		localStorage.setItem('chat_log', JSON.stringify(this.Master));
+	}
 }
